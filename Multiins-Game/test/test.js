1542a76dd852048517373fd7bdd4ec5e16e445c4
wowuliao11 <wowuliao@outlook.com>
2020-10-15 10:32:05 +0800

    BugFix: Delete test.js Promise not used in

--- Multiins-Game/test/test.js
+++ Multiins-Game/test/test.js
@@ -1,4 +1,4 @@
-
+// should
 const should = require('should')
 // require
 const request = require('request')
@@ -14,6 +14,7 @@ describe('test number guess game ', () => {
             done()
         })
     })
+
     it('test page Guess number', (done) => {
         for (let i = 0; i < 100; i += 1) {
             request(`${api}${i}`, (err, res, body) => {
@@ -26,35 +27,35 @@ describe('test number guess game ', () => {
         }
         done()
     })
+
     it('test game playing', (done) => {
         function playgame(min, max) {
-            const p = new Promise((resolve, reject) => {
-                let num = Math.floor((min + max) / 2)
-                let tempmin = min
-                let tempmax = max
-                request(`${api}${num}`, (err, res, body) => {
-                    if (err) {
-                        reject(new Error())
-                        throw err
-                    }
-                    if (body === 'smaller') {
-                        tempmin = num
-                        num = Math.floor((max + min) / 2)
-                        // console.log(`guess:${num}`)
-                        playgame(tempmin, max)
-                    }
-                    if (body === 'bigger') {
-                        tempmax = num
-                        num = Math.floor((max + min) / 2)
-                        // console.log(`guess:${num}`)
-                        playgame(min, tempmax)
-                    }
-                    if (body === 'equal') {
-                        // console.log(`guess:${num}`)
-                        done()
-                    }
-                })
+            let num = Math.floor((min + max) / 2)
+            let tempmin = min
+            let tempmax = max
+            request(`${api}${num}`, (err, res, body) => {
+                if (err) {
+                    reject(new Error())
+                    throw err
+                }
+                if (body === 'smaller') {
+                    tempmin = num
+                    num = Math.floor((max + min) / 2)
+                    // console.log(`guess:${num}`)
+                    playgame(tempmin, max)
+                }
+                if (body === 'bigger') {
+                    tempmax = num
+                    num = Math.floor((max + min) / 2)
+                    // console.log(`guess:${num}`)
+                    playgame(min, tempmax)
+                }
+                if (body === 'equal') {
+                    // console.log(`guess:${num}`)
+                    done()
+                }
             })
+
         }
         playgame(0, 101)
     })
