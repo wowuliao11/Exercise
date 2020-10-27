3981c55b309efe3d903a771d29ec9ca01ab0845f
wowuliao11 <wowuliao@outlook.com>
2020-10-27 15:10:27 +0800

    Update: Use the global variable of express to transfer the parameter db

--- MongoDBProject/GameSys.js
+++ MongoDBProject/GameSys.js
@@ -2,141 +2,137 @@ const express = require('express')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
 const bodyParser = require('body-parser')
-
+const { MongoClient } = require('mongodb')
 const service = require('./service.js')
 
 const urllencodedParser = bodyParser.urlencoded({ extended: false })
 const app = express()
 const uri = 'mongodb://root:admin@localhost:10086/game?retryWrites=true&w=majority'
 
-const dbpool = require('./dbpool')
-
-dbpool.acquire().then((client) => {
-	const db = client.db('game')
-
-	const MongoStoreIns = new MongoStore({
-		db,
-		url: uri,
+MongoClient.connect(uri, { useUnifiedTopology: true })
+	.then((client) => {
+		const db = client.db('game')
+		app.locals.db = db
 	})
 
-	app.use('/', express.static('pages'))
+const MongoStoreIns = new MongoStore({
+	db: app.locals.db,
+	url: uri,
+})
 
-	app.use(session({
-		secret: 'foo',
-		rolling: true,
-		saveUninitialized: false,
-		resave: false,
-		store: MongoStoreIns,
-		cookie: {
-			maxAge: 1000 * 300,
-		},
-		autoRemove: 'native',
-	}))
-	// ----------------------------------listen-------------------------------------
-	app.use('/destroy', (request, response) => {
-		service.deleteNumber(request.session.id)
-		request.session.destroy()
-		response.end('destroy')
-	})
+app.use('/', express.static('pages'))
 
-	app.use('/login', urllencodedParser, (request, response) => {
-		if (!request.body) return response.sendStatus(400)
-		const { name } = request.body
-		const { password } = request.body
-		if (name === undefined || password === undefined) {
-			response.end('data undefined')
-		} else if (name === '' || password === '') {
-			response.end('coldn\'t empty')
-		} else if (name.length < 3 || name.length > 14
-			|| password.length < 3 || password.length > 14) {
-			response.end('bad format')
-		}
-
-		service.findUser(name)
-			.then((result) => {
-				if (result === false) {
-					response.end('account is not exist')
-				}
-				return service.judgeLogin(name, password)
-			})
-			.then((result) => {
-				if (result === false) {
-					response.end('wrong password')
-				} else {
-					request.session.name = name
-					response.end(`Hello ${name}`)
-				}
-			})
-		return null
-	})
-
-	app.use('/register', urllencodedParser, (request, response) => {
-		if (!request.body) return response.sendStatus(400)
-		const { name } = request.body
-		const { password } = request.body
-
-		if (name === '' || password === '') {
-			response.end('coldn\'t empty')
-		} else if (name.length < 3 || name.length > 14
-			|| password.length < 3 || password.length > 14) {
-			response.end('bad format')
-		}
-
-		service.findUser(name).then((result) => {
-			console.log(`findUser 's result:${result}`)
-			if (result === true) {
-				response.end('Duplicate ID')
+app.use(session({
+	secret: 'foo',
+	rolling: true,
+	saveUninitialized: false,
+	resave: false,
+	store: MongoStoreIns,
+	cookie: {
+		maxAge: 1000 * 300,
+	},
+	autoRemove: 'native',
+}))
+// ----------------------------------listen-------------------------------------
+app.use('/destroy', (request, response) => {
+	service.deleteNumber(app.locals.db, request.session.id)
+	request.session.destroy()
+	response.end('destroy')
+})
+app.use('/login', urllencodedParser, (request, response) => {
+	if (!request.body) return response.sendStatus(400)
+	const { name } = request.body
+	const { password } = request.body
+	if (name === undefined || password === undefined) {
+		response.end('data undefined')
+	} else if (name === '' || password === '') {
+		response.end('coldn\'t empty')
+	} else if (name.length < 3 || name.length > 14
+		|| password.length < 3 || password.length > 14) {
+		response.end('bad format')
+	}
+	service.findUser(app.locals.db, name)
+		.then((result) => {
+			if (result === false) {
+				response.end('account is not exist')
+			}
+			return service.judgeLogin(app.locals.db, name, password)
+		})
+		.then((result) => {
+			if (result === false) {
+				response.end('wrong password')
 			} else {
-				service.insertUser(name, password)
-				response.end('success create user!')
+				request.session.name = name
+				response.end(`Hello ${name}`)
 			}
 		})
-		return null
-	})
+	return null
+})
 
-	app.use('/start', (request, response) => {
-		const { name } = request.session
-		const { id } = request.session
-		if (id === undefined || name === undefined) {
-			response.end('Not logged in')
+app.use('/register', urllencodedParser, (request, response) => {
+	if (!request.body) return response.sendStatus(400)
+	const { name } = request.body
+	const { password } = request.body
+
+	if (name === '' || password === '') {
+		response.end('coldn\'t empty')
+	} else if (name.length < 3 || name.length > 14
+		|| password.length < 3 || password.length > 14) {
+		response.end('bad format')
+	}
+
+	service.findUser(app.locals.db, name).then((result) => {
+		if (result === true) {
+			response.end('Duplicate ID')
 		} else {
-			console.log(`name:${name} id:${id}`)
-			const data = Math.floor(Math.random() * 1000000)
-			service.insertNumber(data, id)
-			console.log(`randnum is:${data}`)
-			response.end('OK')
+			service.insertUser(app.locals.db, name, password)
+			response.end('success create user!')
 		}
 	})
-
-	app.use('/:number', (request, response, next) => {
-		const { name } = request.session
-		const { id } = request.session
-		const inputnumber = Number(request.params.number)
-		service.getNumber(id)
-			.then((result) => {
-				if (id === undefined || name === undefined) {
-					response.end('Not logged in')
-				} else if (!result) {
-					response.end('without start')
-				}
-				return service.getNumber(id)
-			})
-			.then((result) => {
-				const r = result
-				if (Number.isNaN(inputnumber) || Number.isNaN(r)) {
-					response.end('input not a number')
-				} else if (inputnumber > r) {
-					response.end('bigger')
-				} else if (inputnumber < r) {
-					response.end('smaller')
-				}
-				response.end('equal')
-			})
-			.catch((err) => {
-				response.end()
-				next(err)
-			})
-	})
-
-	app.listen(8080)
+	return null
 })
+
+app.use('/start', (request, response) => {
+	const { name } = request.session
+	const { id } = request.session
+	if (id === undefined || name === undefined) {
+		response.end('Not logged in')
+	} else {
+		const data = Math.floor(Math.random() * 1000000)
+		service.insertNumber(app.locals.db, data, id)
+		console.log(`randnum is:${data}`)
+		response.end('OK')
+	}
+})
+
+app.use('/:number', (request, response, next) => {
+	const { name } = request.session
+	const { id } = request.session
+	const inputnumber = Number(request.params.number)
+	service.getNumber(app.locals.db, id)
+		.then((result) => {
+			if (id === undefined || name === undefined) {
+				response.end('Not logged in')
+			} else if (!result) {
+				response.end('without start')
+			}
+			return service.getNumber(app.locals.db, id)
+		})
+		.then((result) => {
+			const r = result
+			if (Number.isNaN(inputnumber) || Number.isNaN(r)) {
+				response.end('input not a number')
+			} else if (inputnumber > r) {
+				response.end('bigger')
+			} else if (inputnumber < r) {
+				response.end('smaller')
+			}
+			response.end('equal')
+		})
+		.catch((err) => {
+			response.end()
+			next(err)
+		})
+})
+
+app.listen(8080)
--- MongoDBProject/service.js
+++ MongoDBProject/service.js
@@ -1,157 +1,114 @@
 const md5 = require('md5-node')
 const dao = require('./dao')
-const dbpool = require('./dbpool')
 
 // const db = dao.getDb(uri, 'game')
 
-function insertUser(name, password) { // insert a User
+// const resoursePro = dbpool.acquire()
+function insertUser(db, name, password) { // insert a User
 	if (name === null || password === null) throw new Error('wrong input')
 
-	const resoursePro = dbpool.acquire()
-	resoursePro.then((client) => {
-		const salt = Math.floor(Math.random() * 10000000)
-		const ciphertext = md5(name + salt + password)
-		dao.insertDocument(client.db('game'), 'user', { name, salt, password: ciphertext })
-			.then((result) => {
-				console.log(result)
-				dbpool.release(client)
-			})
-			.catch((err) => {
-				console.log(err)
-				dbpool.release(client)
-			})
-	})
-}
-
-function findUser(name) { // find user if live
-	if (name === null) throw new Error('wrong input')
-
-	const resoursePro = dbpool.acquire()
-	return resoursePro.then((client) => dao.findDocument(client.db('game'), 'user', { name })
+	const salt = Math.floor(Math.random() * 10000000)
+	const ciphertext = md5(name + salt + password)
+	dao.insertDocument(db, 'user', { name, salt, password: ciphertext })
 		.then((result) => {
-			dbpool.release(client)
-			return !!result.length
-		})
-		.catch((err) => {
-			dbpool.release(client)
-			console.log(err)
-		}))
-}
-function findNumber(_id) { // find number if live filter is _id
-	if (_id === null) throw new Error('wrong input')
-
-	const resoursePro = dbpool.acquire()
-	return resoursePro.then((client) => dao.findDocument(client.db('game'), 'number', { _id })
-		.then((result) => {
-			dbpool.release(client)
-			return !!result.length
-		})
-		.catch((err) => {
-			dbpool.release(client)
-			console.log(err)
-		}))
-}
-function getNumber(_id) { // get number in Number filter is _id
-	if (_id === null) throw new Error('wrong input')
-
-	const resoursePro = dbpool.acquire()
-	return resoursePro.then((client) => dao.findDocument(client.db('game'), 'number', { _id })
-		.then((result) => {
-			dbpool.release(client)
-			return result[0] ? result[0].number : null
-		})
-		.catch((err) => {
-			dbpool.release(client)
-			console.log(err)
-		}))
-}
-function judgeLogin(name, password) { // judge longin infomation
-	if (findUser(name) === false) throw new Error('wrong input')
-
-	const resoursePro = dbpool.acquire()
-	return resoursePro.then((client) => dao.findDocument(client.db('game'), 'user', { name })
-		.then((result) => {
-			dbpool.release(client)
-			return result[0].password === md5(name + result[0].salt + password)
-		})
-		.catch((err) => {
-			dbpool.release(client)
-			console.log(err)
-		}))
-}
-function insertNumber(number, _id) { // insert number
-	if (number === null || _id === null) throw new Error('wrong input')
-
-	const resoursePro = dbpool.acquire()
-	return resoursePro.then((client) => dao.insertDocument(client.db('game'), 'number', { _id, number })
-		.then((result) => {
-			dbpool.release(client)
 			console.log(result)
 		})
 		.catch((err) => {
-			dbpool.release(client)
 			console.log(err)
-		}))
+		})
 }
-function deleteNumber(_id) { // delete number
-	if (_id === null) throw new Error('wrong input')
 
-	const resoursePro = dbpool.acquire()
-	return resoursePro.then((client) => {
-		dao.findDocument(client.db('game'), 'number', { _id })
-			.then((result) => {
-				if (!result.length) {
-					throw new Error('isnt this user\'s number')
-				}
-				return dao.deleteDocument(client.db('game'), 'number', { _id })
-			})
-			.then((result) => {
-				dbpool.release(client)
-				console.log(result)
-			})
-			.catch((err) => {
-				dbpool.release(client)
-				console.log(err)
-			})
-	})
-}
-function deleteUser(name) { // delete number
+function findUser(db, name) { // find user if live
 	if (name === null) throw new Error('wrong input')
 
-	const resoursePro = dbpool.acquire()
-	return resoursePro.then((client) => {
-		dao.findDocument(client.db('game'), 'user', { name })
-			.then((result) => {
-				if (!result.length) {
-					throw new Error('isnt this user\'s user')
-				}
-				return dao.deleteDocument(client.db('game'), 'user', { name })
-			})
-			.then((result) => {
-				dbpool.release(client)
-				console.log(result)
-			})
-			.catch((err) => {
-				dbpool.release(client)
-				console.log(err)
-			})
-	})
+	return dao.findDocument(db, 'user', { name })
+		.then((result) => !!result.length)
+		.catch((err) => {
+			console.log(err)
+		})
 }
-function updateNumber(_id, number) { // update number
+function findNumber(db, _id) { // find number if live filter is _id
+	if (_id === null) throw new Error('wrong input')
+
+	return dao.findDocument(db, 'number', { _id })
+		.then((result) => !!result.length)
+		.catch((err) => {
+			console.log(err)
+		})
+}
+function getNumber(db, _id) { // get number in Number filter is _id
+	if (_id === null) throw new Error('wrong input')
+
+	return dao.findDocument(db, 'number', { _id })
+		.then((result) => (result[0] ? result[0].number : null))
+		.catch((err) => {
+			console.log(err)
+		})
+}
+function judgeLogin(db, name, password) { // judge longin infomation
+	if (findUser(db, name) === false) throw new Error('wrong input')
+
+	return dao.findDocument(db, 'user', { name })
+		.then((result) => result[0].password === md5(name + result[0].salt + password))
+		.catch((err) => {
+			console.log(err)
+		})
+}
+function insertNumber(db, number, _id) { // insert number
+	if (number === null || _id === null) throw new Error('wrong input')
+
+	return dao.insertDocument(db, 'number', { _id, number })
+		.then((result) => {
+			console.log(result)
+		})
+		.catch((err) => {
+			console.log(err)
+		})
+}
+function deleteNumber(db, _id) { // delete number
+	if (_id === null) throw new Error('wrong input')
+
+	dao.findDocument(db, 'number', { _id })
+		.then((result) => {
+			if (!result.length) {
+				throw new Error('isnt this user\'s number')
+			}
+			return dao.deleteDocument(db, 'number', { _id })
+		})
+		.then((result) => {
+			console.log(result)
+		})
+		.catch((err) => {
+			console.log(err)
+		})
+}
+function deleteUser(db, name) { // delete number
+	if (name === null) throw new Error('wrong input')
+
+	dao.findDocument(db, 'user', { name })
+		.then((result) => {
+			if (!result.length) {
+				throw new Error('isnt this user\'s user')
+			}
+			return dao.deleteDocument(db, 'user', { name })
+		})
+		.then((result) => {
+			console.log(result)
+		})
+		.catch((err) => {
+			console.log(err)
+		})
+}
+function updateNumber(db, _id, number) { // update number
 	if (_id === null || number === null) throw new Error('wrong input')
 
-	const resoursePro = dbpool.acquire()
-	return resoursePro.then((client) => {
-		dao.updateDocument(client.db('game'), 'number', { _id }, { _id, number })
-			.then(() => {
-				dbpool.release(client)
-				console.log('success updatenumber')
-			})
-			.catch((err) => {
-				dbpool.release(client)
-				console.log(err)
-			})
-	})
+	dao.updateDocument(db, 'number', { _id }, { _id, number })
+		.then(() => {
+			console.log('success updatenumber')
+		})
+		.catch((err) => {
+			console.log(err)
+		})
 }
 
 exports.getNumber = getNumber
